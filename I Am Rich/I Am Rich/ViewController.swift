5b09632c5173f0c91505d9592e11940f0c5662d6
dinuka-rp <drpiyadigama@gmail.com>
2022-01-27 08:37:58 +0530

    create a basic app with an image and text

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Dinuka Piyadigama on 2022-01-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
